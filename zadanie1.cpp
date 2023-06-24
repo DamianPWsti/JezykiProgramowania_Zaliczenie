8602fe456cd4cbcea8c18cff53c44698d4b910dd
Damian Potoczny <it.potoczny.damian@gmail.com>
2023-06-24 18:10:51 +0200

    First Commit

---
+++ zadanie1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "WSTI" << " --GRUPA-- " << 2 << "DZI " << "Damian" << " " << "Potoczny" << endl;
+    cout << "Zaliczenie "<< " " << "Zadanie 1"<< endl;
+
+system("pause");
+}
